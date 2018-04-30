85f42daff13d52969b30dae926d6e7ac25449b22
kanimozhi0112 <37370569+kanimozhi0112@users.noreply.github.com>
2018-04-30 11:34:04 +0530

    Create select 2 details.sql

---
+++ select 2 details.sql
@@ -0,0 +1,14 @@
+create table students(name varchar(20),roll_no varchar(20),college_name varchar(20));
+insert into students values('kanimozhi','15bec028','ece');
+insert into students values('cathrin','15bec001','ece');
+insert into students values('akshaya','15bec044','ece');
+insert into students values('kaviya','15bec042','ece');
+insert into students values('soorya','15bec023','ece');
+insert into students values('malavika','15bec007','ece');
+insert into students values('arina','15bec125','ece');
+insert into students values('pramitha','15bec041','ece');
+insert into students values('deva','15bec026','ece');
+insert into students values('arivu','15bec027','ece');
+select * from students where name='kanimozhi';
+select * from students where college_name='ece';
+
